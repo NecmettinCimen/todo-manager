f5cc66a50dc4629c0c9ac6aa6b49c1f2dfb7764b
Necmettin Cimen <necmettincimen01@gmail.com>
2019-06-16 11:17:45 +0300

    first"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
+import React from 'react'
+import {
+  Container,
+  Dropdown,
+  Image,
+  List,
+  Menu,
+  Segment
+} from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 import logo from './logo.svg';
-import './App.css';
+import './index.css'
+import ContainerChild from './components/ContainerChild'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const FixedMenuLayout = () => (
+  <div className='Site'>
+    <Menu fixed='top' inverted>
+      <Container>
+        <Menu.Item as='a' header>
+          <Image size='mini' src={logo} style={{ marginRight: '1.5em' }} />
+          RTM
+        </Menu.Item>
+        <Menu.Item as='a'>Home</Menu.Item>
 
-export default App;
+        <Dropdown item simple text='Dropdown'>
+          <Dropdown.Menu>
+            <Dropdown.Item>List Item</Dropdown.Item>
+            <Dropdown.Item>List Item</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Header>Header Item</Dropdown.Header>
+            <Dropdown.Item>
+              <i className='dropdown icon' />
+              <span className='text'>Submenu</span>
+              <Dropdown.Menu>
+                <Dropdown.Item>List Item</Dropdown.Item>
+                <Dropdown.Item>List Item</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown.Item>
+            <Dropdown.Item>List Item</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Container>
+    </Menu>
+
+    <Container className='Site-content' text style={{ marginTop: '7em', flex: 1 }}>
+      <ContainerChild />
+    </Container>
+
+    <Segment inverted vertical style={{ margin: '5em 0em 0em', backgroundColor: 'black' }}>
+      <Container textAlign='center'>
+        <List horizontal inverted divided link size='small'>
+          <List.Item as='a' href='#'>
+            Site Map
+          </List.Item>
+          <List.Item as='a' href='#'>
+            Contact Us
+          </List.Item>
+          <List.Item as='a' href='#'>
+            Terms and Conditions
+          </List.Item>
+          <List.Item as='a' href='#'>
+            Privacy Policy
+          </List.Item>
+        </List>
+      </Container>
+    </Segment>
+  </div>
+)
+
+export default FixedMenuLayout
---
+++ src/components/ContainerChild.js
@@ -0,0 +1,132 @@
+import React, { Component } from "react";
+import { Header, Input, Icon, Table, Label } from "semantic-ui-react";
+import moment from "moment";
+
+export default class ContainerChild extends Component {
+  state = {
+    todoName: "",
+    table: []
+  };
+  constructor(props) {
+    super(props);
+
+    let table = JSON.parse(localStorage.getItem("table"));
+
+    if (table) this.state.table = table;
+
+    this.handleChange = this.handleChange.bind(this);
+    this.onRemoveTodo = this.onRemoveTodo.bind(this);
+    this.onUpdateTodo = this.onUpdateTodo.bind(this);
+  }
+  onAddTodoForm = event => {
+    event.preventDefault();
+    this.onAddTodo();
+  };
+  onAddTodo = () => {
+    const { todoName, table } = this.state;
+    if (todoName) {
+      table.push({ status: 1, name: todoName, date: Date.now() });
+      this.setState({ todoName: "" });
+      this.saveTable(table);
+    }
+  };
+  onRemoveTodo = date => {
+    const { table } = this.state;
+    let tableNew = table.filter(x => x.date !== date);
+    this.saveTable(tableNew);
+  };
+  onUpdateTodo = date => {
+    const { table } = this.state;
+
+    let index = table.findIndex(x => x.date === date);
+    if (table[index].status === 1) table[index].status = 2;
+    else table[index].status = 1;
+
+    this.saveTable(table);
+  };
+  saveTable = table => {
+    this.setState({ table });
+    localStorage.setItem("table", JSON.stringify(table));
+  };
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+  render() {
+    const { todoName, table } = this.state;
+    return (
+      <div>
+        <Header as="h1">React Todo Manager</Header>
+        <form onSubmit={this.onAddTodoForm}>
+          <Input
+            value={todoName}
+            name="todoName"
+            onChange={this.handleChange}
+            icon={
+              <Icon
+                onClick={() => this.onAddTodo()}
+                name="add"
+                inverted
+                circular
+                link
+              />
+            }
+            fluid
+            placeholder="Todo Name..."
+          />
+        </form>
+        <Table celled>
+          <Table.Body>
+            {table.map(({ name, date, status }) => (
+              <TableRow
+                key={"row_" + date}
+                name={name}
+                date={date}
+                status={status}
+                onUpdateTodo={this.onUpdateTodo}
+                onRemoveTodo={this.onRemoveTodo}
+              />
+            ))}
+          </Table.Body>
+        </Table>
+      </div>
+    );
+  }
+}
+
+const TableStatuses = {
+  1: { name: "Added", color: "yellow" },
+  2: { name: "Completed", color: "blue" }
+};
+
+const TableRow = ({ name, date, status, onRemoveTodo, onUpdateTodo }) => (
+  <Table.Row>
+    <Table.Cell collapsing>
+      <Label
+        as="a"
+        basic
+        color={TableStatuses[status].color}
+        onClick={() => onUpdateTodo(date)}
+      >
+        {TableStatuses[status].name}
+      </Label>
+    </Table.Cell>
+    <Table.Cell>{name}</Table.Cell>
+    <Table.Cell title={moment(date).fromNow()} collapsing>
+      <Label color="violet">
+        {moment(date)
+          .format("DD-MM-YYYY HH:mm")
+          .replace(moment().format("DD-MM-YYYY "), "")}
+      </Label>
+    </Table.Cell>
+    <Table.Cell collapsing>
+      <Icon
+        onClick={() => onRemoveTodo(date)}
+        name="remove"
+        inverted
+        circular
+        link
+      />
+    </Table.Cell>
+  </Table.Row>
+);
